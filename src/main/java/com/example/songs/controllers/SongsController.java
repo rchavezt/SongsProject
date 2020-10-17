21b2bf2ea6f071fff0e4ef80123f94608b30ba0d
Rafael Chavez <rafaelchavez@MacBook-Pro-de-Rafael.local>
2020-10-16 22:46:43 -0500

    Primer servicio

--- src/main/java/com/example/songs/controllers/SongsController.java
+++ src/main/java/com/example/songs/controllers/SongsController.java
@@ -1,5 +1,9 @@
 package com.example.songs.controllers;
 
+import com.example.songs.dto.SongDto;
+import com.example.songs.services.Songs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/song")
 public class SongsController {
 
+    @Autowired
+    private Songs songs;
+
     @RequestMapping(value = "/1", method = RequestMethod.GET)
-    public String getSong(){
-        return "In the shadows";
+    public SongDto getSong(){
+        return songs.getSong();
+    }
+
+    @RequestMapping(value = "/{song}", method = RequestMethod.GET)
+    public String getSongUrl(@PathVariable String song){
+        return song;
     }
 }
