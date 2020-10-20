88c9f6348e1d044297e89e766aeadebe0e4416e5
SergioMendoza-max <73005097+SergioMendoza-max@users.noreply.github.com>
2020-10-19 19:33:53 -0500

    Mis cambios

--- src/main/java/com/example/songs/controllers/SongsController.java
+++ src/main/java/com/example/songs/controllers/SongsController.java
@@ -3,10 +3,15 @@ package com.example.songs.controllers;
 import com.example.songs.dto.SongDto;
 import com.example.songs.services.Songs;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 
 @RestController
 @RequestMapping("/song")
@@ -15,13 +20,66 @@ public class SongsController {
     @Autowired
     private Songs songs;
 
+    List<String> song1=new ArrayList<String>();
+
+    public List<String> getSong1() {
+        song1.add("Unchained");
+        song1.add("2Pac/James Brown");
+        song1.add("Django Unchained Soundtrack");
+
+        return song1;
+    }
+
+    List<String> song2=new ArrayList<String>();
+
+    public List<String> getSong2() {
+        song2.add("St Petes");
+        song2.add("SuicideYear");
+        song2.add("Japan");
+
+        return song2;
+    }
+
+    List<String> song3= new ArrayList<String>();
+
+    public List<String> getSong3(){
+        song3.add("Tsurugi no mai");
+        song3.add("Nujabes");
+        song3.add("Samurai Champloo");
+
+        return song3;
+    }
+
+
+    public int getRandom(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+    List<List<String>> amalgama=new ArrayList<>(Collections.emptyList());
+
+    public List<List<String>> getAmalgama(){
+        amalgama.add(song1);
+        amalgama.add(song2);
+        amalgama.add(song3);
+        List<String> cancion=amalgama.get(getRandom(0,3));
+
+        return getAmalgama();
+    }
+
     @RequestMapping(value = "/1", method = RequestMethod.GET)
     public SongDto getSong(){
         return songs.getSong();
     }
 
+
+
     @RequestMapping(value = "/{song}", method = RequestMethod.GET)
     public String getSongUrl(@PathVariable String song){
         return song;
+    }
+
+    @RequestMapping(value = { "/mapping/{param1}" }, method = { RequestMethod.GET})
+    public void mapping(HttpServletResponse response, @PathVariable("param1") String param1, @RequestParam(required = true, value = "https://open.spotify.com/album/5iTTqogtbo5iGG5oqTRZuw") String redirectLink) throws IOException {
+        response.sendRedirect(redirectLink);
     }
 }
--- src/main/java/com/example/songs/dto/SongDto.java
+++ src/main/java/com/example/songs/dto/SongDto.java
@@ -10,3 +10,4 @@ public class SongDto {
     public String artist;
     public String album;
 }
+
